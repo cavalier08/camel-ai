7738a709eccbd6b3a51904cf6582379d2cd4e091
cavalier08 <chantal.zhang2018@gmail.com>
2025-03-01 22:31:48 -0500

    yay

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ import Build from './build';
 
 export default function Home() {
   return (
-    <div className="bg-cover bg-[url('https://e1.pxfuel.com/desktop-wallpaper/71/12/desktop-wallpaper-gaussian-blur-gradient-simple-backgrounds-blurred-colors-color-gradient.jpg')]">
+    <div className="bg-cover bg-[url('https://t3.ftcdn.net/jpg/04/38/04/24/360_F_438042459_X9KgJ6dGUcz2kJElm5ubL5pq7rrcC6wa.jpg')]">
       <div className="flex justify-center items-center">
         <Navbar />
 
